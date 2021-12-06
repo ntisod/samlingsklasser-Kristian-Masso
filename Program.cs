e1ca697f5328e6537eb2913edb23d04037d3a5ab
Kristian-Masso <kristian.masso@elev.ga.ntig.se>
2021-12-06 12:35:27 +0100

    övning 1 klar

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿    using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Samlingsklasser
 {
-    class Program
+    class Program   
     {
         static void Main(string[] args)
         {
@@ -74,7 +74,31 @@ namespace Samlingsklasser
         static void DiceSortedList()
         {
             //Övning 1
+            //Skapa en sortedlist med int som nyckel och int som värde
+            SortedList<int, int> resultat = new SortedList<int, int>();
 
+            //skapa ett Random objekt för att slumpa
+            Random random = new Random();
+
+            //Gör tusen upprepnigar
+            for (int i = 0; i < 1000; i++)
+            {
+                //Slumpa tal mellan 1 och 6
+                int tal = random.Next(1, 7);
+
+                //Lägg nyckel om denna inte redan finns
+                if (!resultat.ContainsKey(tal))
+                    resultat.Add(tal, 0);
+
+                //Öka förekomsten av tal
+                resultat[tal]++;
+            }
+
+            //Visa resultatet
+            foreach (KeyValuePair<int, int> kvp in resultat)
+            {
+                Console.WriteLine("Nyckel: {0} Värde: {1}", kvp.Key, kvp.Value);
+            }
         }
 
         static void DiceDictionary()
