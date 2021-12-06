efb1de73b1b86405cf7d7e31d41a9cde4536fd39
Kristian-Masso <kristian.masso@elev.ga.ntig.se>
2021-12-06 13:38:39 +0100

    övning 2 oliver jobbade med mig men hade problem med att skicka in uppgiften

--- Program.cs
+++ Program.cs
@@ -10,28 +10,27 @@ namespace Samlingsklasser
     {
         static void Main()
         {
-            //Skapar en lista som heter "övning2" och som har värdet double
             List<double> övning2 = new List<double>();
 
             //Skapar en for metod som gör att programmet innanför den skall upprepas
             for (int i = 0; i < 1000000000; i++)
             {
-                //Frågar användaren om värdet på talet som han/hon skall mata in
+                //Frågar om ett värde
                 Console.Write("Skriv in ett tal: ");
-                //Omvandlar svaret från användaren, från string till double
+                //Omvandlar svaret till double
                 double tal = double.Parse(Console.ReadLine());
-                //använder mig av if eftersom det skall finnas ett villkor om användaren trycker in talet 0
+                //använder if för jag ska ange villkor
                 if (tal == 0)
                 {
-                    //Denna rad gör att programmet stängs ner när man matar in 0
+                    //skriver in 0 = programmet stängs av
                     Environment.Exit(1);
                 }
-                //Annars, ifall man sätter in andra värden än 0 då skall programmet innanför else satsen köras
+                //om man skriver nåt annat än 0 så blir det de här
                 else
                 {
-                    //Denna rad gör att talet som blev inmatad av användaren skall lägga till i listan som vi har skapat.
+                    //användares tal läggs in i listan
                     övning2.Add(tal);
-                    //Denna rad skriver ut svaret av själva programmet. Programmet skall skriva ut medelvärdet av talen i listan. 
+                    //skriver ut svaret och medelvärdet på talen från listan
                     Console.WriteLine("Medelvärdet blir: " + övning2.Average());
                 }
             }
